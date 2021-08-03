74f5ff27d1b235cbbce38cf1155558a8f95abb61
Eduardo Gomes <85569960+eduardofgomes@users.noreply.github.com>
2021-08-03 17:48:42 +0000

    03/08 - Continuação da introdução ao jQuery

--- js/load-page.js
+++ js/load-page.js
@@ -5,6 +5,37 @@ $(document).ready(function(){//executar a leitura do documento e preparou uma fu
   $('a').click(function(event){
     event.preventDefault()
 
+    let pagina = $(this).attr('href')
+
+    $('.modal-title').empty()
+    $('.modal-body').empty()
+
+    switch(pagina){
+      case 'instagram':
+        $('.modal-title').append('Meu Instagram')
+        $('.modal-body').append('<p>Instagram.......</p>')
+        break
+      case 'facebook':
+        $('.modal-title').append('Meu Facebook')
+        $('.modal-body').append('<p>Facebook.......</p>')
+        break
+      case 'github':
+        $('.modal-title').append('Meu Github')
+        $('.modal-body').append('<p>GitHub.......</p>')
+        break
+      case 'linkedin':
+        $('.modal-title').append('Linkedin')
+        $('.modal-body').append('<p>Linkedin.......</p>')
+        break
+      case 'twitter':
+        $('.modal-title').append('Meu Twitter')
+        $('.modal-body').append('<p>Twitter.......</p>')
+        break
+      default: alert('Página não encontrada')
+    }
+
     $('#janela').modal('show')
+
+    //$('#janela').modal('show')
   })
 })
