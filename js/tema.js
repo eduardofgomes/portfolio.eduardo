1acbe1a06ef7ac5963c3632d90f6e8293e26135b
Eduardo Gomes <85569960+eduardofgomes@users.noreply.github.com>
2021-08-10 15:30:56 +0000

    Interatividade com jQuery

--- js/tema.js
+++ js/tema.js
@@ -1,3 +1,45 @@
 $(document).ready(function(){
   
+  $('btn-dark').click(function(e){
+    e.preventDefault()
+
+    $('body').removeClass('original')
+    $('body').removeClass('light')
+    $('body').addClass('dark')
+    $('p').addClass('text-white')
+    $('p').removeClass('text-dark')
+    $('h3').addClass('text-white')
+    $("h3").removeClass('text-dark')
+    $("h4").addClass('text-white')
+    $("h4").removeClass('text-dark')
+  })
+
+
+  $('btn-light').click(function(e){
+    e.preventDefault()
+
+    $('body').removeClass('original')
+    $('body').removeClass('dark')
+    $('body').addClass('light')
+    $('p').addClass('text-white')
+    $('p').removeClass('text-dark')
+    $('h3').addClass('text-white')
+    $("h3").removeClass('text-dark')
+    $("h4").addClass('text-white')
+    $("h4").removeClass('text-dark')
+  })
+
+  $('.btn-primary').click(function(e){
+    e.preventDefault()
+
+    $('body').removeClass('light')
+    $('body').removeClass('dark')
+    $('body').addClass('original')
+    $('p').addClass('text-white')
+    $('p').removeClass('text-dark')
+    $('h3').addClass('text-white')
+    $('h3').removeClass('text-dark')
+    $('h4').addClass('text-white')
+    $('h4').removeClass('text-dark')
+  })
 })
